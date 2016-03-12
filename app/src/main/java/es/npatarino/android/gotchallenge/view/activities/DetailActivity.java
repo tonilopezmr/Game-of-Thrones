3e66914ea1bb22648f4b4a3e2e4ce48bca7e0c4c
Antonio López Marín <tonilopezmr@gmail.com>
2016-03-12 02:14:48 +0100

    replace thread for picasso in DetailActivity

--- app/src/main/java/es/npatarino/android/gotchallenge/view/activities/DetailActivity.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/view/activities/DetailActivity.java
@@ -1,16 +1,12 @@
 package es.npatarino.android.gotchallenge.view.activities;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.URL;
+import com.squareup.picasso.Picasso;
 
 import es.npatarino.android.gotchallenge.R;
 
@@ -39,25 +35,8 @@ public class DetailActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                URL url = null;
-                try {
-                    url = new URL(i);
-                    final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                    DetailActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            ivp.setImageBitmap(bmp);
-                            tvn.setText(n);
-                            tvd.setText(d);
-                        }
-                    });
-                } catch (IOException e) {
-                    Log.e(TAG, e.getLocalizedMessage());
-                }
-            }
-        }).start();
+        Picasso.with(getApplicationContext()).load(i).into(ivp);
+        tvn.setText(n);
+        tvd.setText(d);
     }
 }
