523e0a8a261a34605aabe63660a6adbd43a35f63
Antonio López Marín <tonilopezmr@gmail.com>
2016-05-03 21:01:18 +0200

    created tab custon view

--- app/src/main/java/es/npatarino/android/gotchallenge/view/activities/HomeActivity.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/view/activities/HomeActivity.java
@@ -1,10 +1,14 @@
 package es.npatarino.android.gotchallenge.view.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatDrawableManager;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import es.npatarino.android.gotchallenge.R;
 import es.npatarino.android.gotchallenge.view.adapters.SectionsPagerAdapter;
@@ -34,22 +38,23 @@ public class HomeActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_word_list);
+        tabLayout.getTabAt(0).setCustomView(getCustomIcon(R.drawable.ned_head_light));
         tabLayout.getTabAt(0).setText("");
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_group_list);
+        tabLayout.getTabAt(1).setCustomView(getCustomIcon(R.drawable.ned_head_light, 125));
         tabLayout.getTabAt(1).setText("");
-        tabLayout.getTabAt(1).getIcon().mutate().setAlpha(125);
 
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                tab.getIcon().mutate().setAlpha(255);
+                ImageView imageView = (ImageView) tab.getCustomView();
+                imageView.getDrawable().mutate().setAlpha(255);
                 viewPager.setCurrentItem(tab.getPosition(), true);
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                tab.getIcon().mutate().setAlpha(125);
+                ImageView imageView = (ImageView) tab.getCustomView();
+                imageView.getDrawable().mutate().setAlpha(125);
             }
 
             @Override
@@ -57,5 +62,20 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private View getCustomIcon(int resId, int alpha){
+        return getCustomIcon(AppCompatDrawableManager.get().getDrawable(tabLayout.getContext(), resId), alpha);
+    }
+
+    private View getCustomIcon(int resId){
+        return getCustomIcon(AppCompatDrawableManager.get().getDrawable(tabLayout.getContext(), resId), 255);
+    }
+
+    private View getCustomIcon(Drawable icon, int alpha){
+        ImageView imageView = (ImageView) getLayoutInflater().inflate(R.layout.custom_tab, null);
+        imageView.setImageDrawable(icon);
+        imageView.getDrawable().mutate().setAlpha(alpha);
+        return imageView;
     }
 }
